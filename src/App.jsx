fc610831d2959188f5d0bdb99524303432f210d4
Guilherme Borges <guilherme.borges.dev@gmail.com>
2021-03-05 17:49:06 -0300

    Começo da Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import Routes from './routes';
+import './styles/global.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>PrimeDoc Hello World!!!</h1>
+      <Routes />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <main>
+        <h1>Home</h1>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
